b3b0097515bb7b83653bd1516045bc0ee4bedf3e
tails618 <asher.grossman@gmail.com>
2018-10-09 19:06:48 -0500

    Allowed user to choose string

--- outStringThing.py
+++ outStringThing.py
@@ -1,6 +1,6 @@
-string="-><-"
 def outString(x):
     global string
     return string[:2]+x+string[2:]
+string=input("String sting strum star swoosh stop slurp? ")
 word=input("What word would why would when what worm? ")
 print(outString(word))
